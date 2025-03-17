07e121cb43719cfc864cf82a17b8254e8f3e64aa
Memecon <jon.byrne27@btinternet.com>
2025-03-17 21:46:27 +0000

    Added script for triggers

---
+++ Jonathan Byrne - Metaverse VR Test/Assets/Scripts/ObjectiveZoneReached.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectiveZoneReached : MonoBehaviour
+{
+    static bool triggerEntered = false; //this will be accessed in the player script, to check in update that when the player is stationary, if it is in the trigger zone
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player") triggerEntered = true; //will be designated true when entered
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player") triggerEntered = false; //will be designated false when exited
+    }
+}
